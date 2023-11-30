de28d332595399c1fe6390c0ec241b52f9dd0eb6
Grzegorz02@ <grzegorz.perun@gmail.com>
2023-11-30 20:48:49 +0100

    feat: join section cleanup

--- components/join.vue
+++ components/join.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="py-12 sm:px-12 lg:mx-auto lg:max-w-screen-2xl">
-    <p class="text-lg font-medium text-zinc-100">
-      Chcę się zaaangażować...
+    <p class="text-center text-lg font-medium text-red-400">
+      Chcę się zaangażować...
     </p>
-    <h2 class="relative mb-5 text-4xl font-extrabold text-zinc-100 underline decoration-red-400/25 decoration-[1rem] underline-offset-[-0.5rem]" style="text-decoration-skip-ink: none;">
-      Dołącz do nas!
+    <h2 class="relative mb-5 text-center text-4xl font-extrabold text-zinc-100 underline decoration-red-400/25 decoration-[1rem] underline-offset-[-0.5rem] lg:text-5xl" style="text-decoration-skip-ink: none;">
+      Dołącz do nas
     </h2>
-    <p class="mb-5 leading-normal text-zinc-100">
+    <p class="mx-auto mb-5 max-w-screen-md text-center leading-normal text-zinc-100" style="text-wrap: balance;">
       Jeżeli uczysz się programować, to bierz udział w naszych spotkaniach aby dalej poszerzać swoją wiedzę. A jeżeli jesteś już w tym naprawdę dobry, to twórz z nami nowe projekty.
     </p>
     <div class="grid grid-flow-row auto-rows-fr gap-4 lg:grid-flow-col lg:grid-cols-2">
